992d1727fda7bc2769060bdb4b9aa917612ad61a
inbalcohen2 <57721728+inbalcohen2@users.noreply.github.com>
2019-11-15 12:31:11 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -10,9 +10,19 @@ int main(int argc, char const *argv[])
     printf("\n Please inset a real number: ");
     scanf("%lf",&input_x);
 
-double f_1=sub(add(Exp(input_x),Pow(input_x,3)),2);
-double f_2=add(mul(input_x,3),mul(Pow(input_x,2),2));
-double f_3=sub(div(mul(Pow(input_x,3),4),5),mul(input_x,2));
+float tmp1=(float)Exp((int)input_x);
+float tmp2 =(float)Pow(input_x,3);
+float f_1=sub(add(tmp1,tmp2),(float)2);
+
+
+float tmp3=(float)mul(input_x,3);
+float tmp4=(float)mul(Pow(input_x,2),2);
+float f_2=add(tmp3,tmp4);
+
+
+
+float f_3=sub((float)div(mul(Pow(input_x,3),4),5),(float)mul(input_x,2));
+
 printf("The value of f(x)=e^x+x^3−2 at the point %.4f is: %.4f",input_x,f_1);
 printf("The value of f(x)=3x+2X^2 at the point %.4f is: %.4f",input_x,f_2);
 printf("The value of f(x)=(4x^3)/5-2x at the point %.4f is: %.4f",input_x,f_3);
